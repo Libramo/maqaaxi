5dab05a3eeb813179dea7fd005eac7174d9ad0ca
Liban Yonis <assadyonis@gmail.com>
2022-03-18 08:52:30 +0300

    heeelp

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 // const { Sequelize, DataTypes } = require("sequelize");
 const morgan = require("morgan");
