b6e6db98ee8680e922557e6fc24d4c63d90217e9
Liban Yonis <assadyonis@gmail.com>
2022-05-05 15:15:46 +0300

    test

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,25 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = new Pool();
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
+// };
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const prodConfig = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? prodConfig : devConfig,
+});
+
+// const pool = new Pool();
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
+module.exports = pool;
